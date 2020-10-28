ad70f5fbb41e92c1009dd46fdb57537bb1f18671
Micah Silverman <micah@afitnerd.com>
2020-10-28 00:04:51 -0400

    Fixed run script for renamed module. Updated readme. (#3)

--- run-example.sh
+++ run-example.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 
 # exec:java causes sigsegv's on mac with jna
-# mvn exec:java -pl blink1-examples -Dexec.mainClass="com.thingm.blink1.$1" -Dexec.cleanupDaemonThreads=false
 
 java \
--cp blink1-library/target/blink1-library-jar-with-dependencies.jar:./blink1-examples/target/blink1-examples.jar \
-com.thingm.blink1.$1
+-cp blink1-library/target/blink1-library-jar-with-dependencies.jar:./blink1-java-examples/target/blink1-java-examples.jar \
+com.thingm.blink1.$1 $2 $3 $4
