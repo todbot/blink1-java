bf59d87c34607036f06fbdde19096144c6dc67f3
Tod Kurt <tod@todbot.com>
2023-11-24 17:49:00 -0800

    address issue #6 by setting "headless=true" when

--- run-example.sh
+++ run-example.sh
@@ -3,5 +3,6 @@
 # exec:java causes sigsegv's on mac with jna
 
 java \
+-Djava.awt.headless=true \
 -cp blink1-library/target/blink1-library-jar-with-dependencies.jar:./blink1-java-examples/target/blink1-java-examples.jar \
 com.thingm.blink1.$1 $2 $3 $4
